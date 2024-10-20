f83fb69553689aae1965608d2afa055223f65139
ARJUN A A R <142217865+arjunaar2789@users.noreply.github.com>
2024-10-20 10:44:12 +0530

    Update untitled13.py

--- untitled13.py
+++ untitled13.py
@@ -8,8 +8,8 @@ Original file is located at
 """
 
 
-pip install PyPDF2
-pip install sentence_transformers
+!pip install PyPDF2
+!pip install sentence_transformers
 
 import streamlit as st
 import PyPDF2
